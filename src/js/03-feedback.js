12f43069bfd9274d31744039f8b2603afe816033
magdalenajb <m.juszczyk_84@o2.pl>
2023-04-05 14:20:31 +0200

    finish

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -4,35 +4,24 @@ const form = document.querySelector('.feedback-form');
 const email = document.querySelector('input[type="email"]');
 const textarea = document.querySelector('textarea[name="message"]');
 const btn = document.querySelector('button[type="submit"]');
-
 const LOCALSTORAGE_KEY = 'feedback-form-state';
 
-form.addEventListener(
-  'input',
-  throttle(() => {
-    let inputValues = {
-      email: email.value,
-      textarea: textarea.value,
-    };
-    localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(inputValues));
-  }, 500)
+form.addEventListener('input',throttle(() => {let inputValues = {
+email: email.value,
+textarea: textarea.value,
+};
+localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(inputValues));
+ }, 500)
 );
 
-let storageValue = localStorage.getItem(LOCALSTORAGE_KEY);
-
+function stayForm () {
+  const storageValue = localStorage.getItem(LOCALSTORAGE_KEY);
 if (storageValue !== null) {
-  dataForm = { email: email.value, message: message.value };
-  let dataForm = JSON.parse(storageValue);
-  
+    email.value = JSON.parse(storageValue).email;
+    textarea.value = JSON.parse(storageValue).textarea;
+  }
 }
-
-
-
-
-
-
-
-
+stayForm();
 
 form.addEventListener('submit', e => {
   e.preventDefault();
@@ -41,7 +30,7 @@ form.addEventListener('submit', e => {
     email: email.value,
     textarea: textarea.value,
   });
-
   localStorage.removeItem(LOCALSTORAGE_KEY);
   form.reset();
 });
+
